960957600a54d6dc58b01b116b46061344d9320d
Teguh <teguhmad123@gmail.com>
2024-06-23 15:47:05 +0700

    feat : detect cond

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import supervision as sv
 from ultralytics import YOLOv10
 
+import cv2
 from helpers.camera_helper import CameraHelper
 # from helpers.serial_helper import SerialHelper
 from lib.callback import Log
@@ -13,6 +14,8 @@ class App:
         self.model = YOLOv10(f'assets/model/best.pt')
         self.bounding_box_annotator = sv.BoundingBoxAnnotator()
         self.label_annotator = sv.LabelAnnotator()
+        self.tracker = sv.ByteTrack()
+        self.tracker2 = sv.ByteTrack()
 
         self.log = Log()
         self.log.add_log_server()
@@ -48,16 +51,18 @@ class App:
         frame = self.camera1.capture_frame()
         results = self.model(frame)[0]
         detections = sv.Detections.from_ultralytics(results)
+        detections = self.tracker.update_with_detections(detections)
 
-        if 'coca-cola' in detections.data['class_name']:
-            self.log.add_log(1)
-        # else:
-            # self.log.add_log(0)
+        labels = [
+            f"#{tracker_id} {confidence:.2f} {results.names[class_id]}"
+            for class_id, confidence, tracker_id
+            in zip(detections.class_id, detections.confidence, detections.tracker_id)
+        ]
 
         annotated_image = self.bounding_box_annotator.annotate(
             scene=frame, detections=detections)
         annotated_image = self.label_annotator.annotate(
-            scene=annotated_image, detections=detections)
+            scene=annotated_image, detections=detections, labels=labels)
 
         # Convert the frame from OpenCV to PIL format
         rgb_frame = self.camera1.rgb_frame(frame)
@@ -72,18 +77,20 @@ class App:
         
         # Read a frame from the video capture
         frame2 = self.camera2.capture_frame()
-        results2 = self.model(frame2)[0]
+        results2 = self.model(frame2, conf=0.6)[0]
         detections2 = sv.Detections.from_ultralytics(results2)
+        detections2 = self.tracker2.update_with_detections(detections2)
 
-        if 'coca-cola' in detections2.data['class_name']:
-            self.log.add_log(1)
-        # else:
-            # self.log.add_log(0)
+        labels2 = [
+            f"#{tracker_id} {confidence:.2f} {results2.names[class_id]}"
+            for class_id, confidence, tracker_id
+            in zip(detections2.class_id, detections2.confidence, detections2.tracker_id)
+        ]
 
         annotated_image2 = self.bounding_box_annotator.annotate(
             scene=frame2, detections=detections2)
         annotated_image2 = self.label_annotator.annotate(
-            scene=annotated_image2, detections=detections2)
+            scene=annotated_image2, detections=detections2, labels=labels2)
 
         # Convert the frame from OpenCV to PIL format
         rgb_frame2 = self.camera2.rgb_frame(frame2)
@@ -93,6 +100,19 @@ class App:
         # Update the label with the new frame
         self.video_label2.imgtk = imgtk2
         self.video_label2.configure(image=imgtk2)
+
+        # condition
+
+        if 'class_name' in detections.data and 'class_name' in detections2.data:
+            if 'bersih' in detections.data['class_name'] and 'bersih' in detections2.data['class_name']:
+                self.log.add_log(1)
+            elif 'coretan' in detections.data['class_name'] and 'coretan' in detections2.data['class_name']:
+                self.log.add_log(0)
+            elif 'tercoret' in detections.data['class_name'] and 'tercoret' in detections2.data['class_name']:
+                self.log.add_log(0)
+            elif 'terlipat' in detections.data['class_name'] and 'terlipat' in detections2.data['class_name']:
+                self.log.add_log(0)
+
 
         # Schedule the next update
         self.window.after(10, self.update)
